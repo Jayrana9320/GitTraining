2e028203b06eec960d3e2c0f1391069043ef7b8e
Jay M <jay_rana19@yahoo.com>
2023-01-19 22:07:43 -0500

    corejava added

---
+++ coreJava/src/coreJava/CharArray.java
@@ -0,0 +1,18 @@
+package coreJava;
+
+public class CharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char [] charArr = { 'J', 'a', 'v', 'a'   }; 
+//		for(char c : charArr) {
+//			System.out.println(c + ' ');
+//			  
+//			
+//		}
+		String str = new String(charArr);
+		System.out.println(str);
+
+	}
+
+}
